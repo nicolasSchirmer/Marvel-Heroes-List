9b174c688d65f33ac67bbb7328ff1ad9d117403e
Nícolas Meira da Silva Schirmer <nschirmer.dev@gmail.com>
2018-03-12 01:06:08 -0300

    Heroes acitivty init

--- app/src/main/java/com/nschirmer/marvelheroes/activities/HeroesListActivity.java
+++ app/src/main/java/com/nschirmer/marvelheroes/activities/HeroesListActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nschirmer.marvelheroes.R;
+import com.nschirmer.marvelheroes.utils.ActivityUtils;
 
 public class HeroesListActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class HeroesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes_list);
+        ActivityUtils.setStrechActivity(this);
+
+
     }
 }
--- app/src/main/java/com/nschirmer/marvelheroes/activities/SplashScreenActivity.java
+++ app/src/main/java/com/nschirmer/marvelheroes/activities/SplashScreenActivity.java
@@ -10,6 +10,11 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        try {
+            Thread.sleep(300);
+
+        }catch (InterruptedException e) {}
+
         startActivity(new Intent(this, HeroesListActivity.class));
         finish();
     }
